0612635ef78b287064a0ce2b4b038381bba1c2b1
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2023-01-16 01:15:05 -0300

    modificações em objetos

--- herança/index.php
+++ herança/index.php
@@ -9,18 +9,30 @@
 <body>
     <pre>
         <?php
+            require_once "pessoa.php";
             require_once "aluno.php";
             require_once "professor.php";
             require_once "funcionario.php";
+            
             $p1 = new Aluno();
             $p2 = new Professor();
             $p3 = new Funcionario();
-            $p1->setNome("Esdras");
-            $p2->setNome("Gilmara");
-            $p3->setNome("Maria");
-            print_r($p1);
-            print_r($p2);
-            print_r($p3);
+           
+            /*//$p1->setNome("Esdras");
+            //$p2->setNome("Gilmara");
+           //$p3->setNome("Maria");
+            
+            $p1->setSexo("M");
+            $p2->setSexo("F");
+            $p3->setSexo("F");
+
+            $p1->setCurso("POO em php");
+            $p2->setEspecialidade("Pedagogia");
+            $p3->setSetor("Secretaria");
+            
+            //print_r($p1);
+            //print_r($p2);
+            //print_r($p3);*/
         ?>
     </pre>
 </body>
